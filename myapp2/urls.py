86da459802057cccd34613461a80cc1f3fc38789
Ryo Chiba <rchiba8030@gmail.com>
2021-06-03 17:08:58 +0900

    11

--- myapp2/urls.py
+++ myapp2/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'myapp2'
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('ajax_post/', views.ajax_post, name='ajax_post'),
+	path('ajax_datatable/', views.ajax_datatable, name='ajax_datatable'),
 ]
